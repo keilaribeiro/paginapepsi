c82907a28099ae0564feaf3e01e8f9266aa33012
keilaribeiro <keila.ribeiro.kell@gmail.com>
2021-05-30 16:24:58 -0300

    segundo commit

---
+++ style.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgcolor(color){
+    const se = document.querySelector('.se');
+    se.style.background = color;
+}
